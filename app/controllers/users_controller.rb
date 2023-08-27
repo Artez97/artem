3ad2951081668bdcece9006b3bbca72c678d8592
Artez97 <ermilin-tema@.mail.ru>
2023-08-27 11:01:54 +0300

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,
+                only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -19,6 +21,9 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  def index
+    @users = User.all
+  end
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -35,8 +40,14 @@ class UsersController < ApplicationController
   end
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_url
     end
   end
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
+end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,6 +22,11 @@ module SessionsHelper
     end
   end
   # Returns true if the user is logged in, false otherwise.
+  # # Returns true if the given user is the current user.
+  def current_user?(user)
+   user && user == current_user
+  end
+  # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
   end
@@ -31,5 +36,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
